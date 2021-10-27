6c7bb84a527161c95022004460149809d2741d8c
douglasdl <douglas_san@hotmail.com>
2021-10-27 12:01:46 -0300

    Add functions to check for matches

--- App.js
+++ App.js
@@ -17,6 +17,70 @@ const windowHeight = Dimensions.get('window').height;
 export default function App() {
     const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
 
+    const checkForColumnOfFour = () => {
+        for (let i = 0; i <= (width*width-2*width-1); i++) {
+            const columnOfFour = [i, i + width, i + width * 2, i + width * 3];
+            const decidedColor = currentColorArrangement[i];
+            //const isBlank = currentColorArrangement[i] === blank
+            //&& !isBlank
+            if (columnOfFour.every(square => currentColorArrangement[square] === decidedColor )) {
+                //setScoreDisplay((score) => score + 4)
+                columnOfFour.forEach(square => currentColorArrangement[square] = 'rgba(0, 0, 0, 0)');
+                //return true
+            }
+        }
+    }
+
+    const checkForRowOfFour = () => {
+        for (let i = 0; i < width*width; i++) {
+            const rowOfFour = [i, i + 1, i + 2, i + 3]
+            const decidedColor = currentColorArrangement[i]
+            const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 62, 63, 64]
+            //const isBlank = currentColorArrangement[i] === blank
+
+            if (notValid.includes(i)) continue
+
+            // && !isBlank
+            if (rowOfFour.every(square => currentColorArrangement[square] === decidedColor )) {
+                //setScoreDisplay((score) => score + 4)
+                rowOfFour.forEach(square => currentColorArrangement[square] = 'rgba(0, 0, 0, 0)');
+                //return true
+            }
+        }
+    }
+
+    const checkForColumnOfThree = () => {
+        for (let i = 0; i <= (width*width-3*width-1); i++) {
+            const columnOfThree = [i, i + width, i + width * 2]
+            const decidedColor = currentColorArrangement[i]
+            //const isBlank = currentColorArrangement[i] === blank
+            // && !isBlank
+            if (columnOfThree.every(square => currentColorArrangement[square] === decidedColor )) {
+                //setScoreDisplay((score) => score + 3)
+                columnOfThree.forEach(square => currentColorArrangement[square] = 'rgba(0, 0, 0, 0)')
+                //return true
+            }
+        }
+    }
+
+    const checkForRowOfThree = () => {
+        for (let i = 0; i < 64; i++) {
+            const rowOfThree = [i, i + 1, i + 2]
+            const decidedColor = currentColorArrangement[i]
+            const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64]
+            //const isBlank = currentColorArrangement[i] === blank
+
+            if (notValid.includes(i)) continue
+            // && !isBlank
+            if (rowOfThree.every(square => currentColorArrangement[square] === decidedColor )) {
+                //setScoreDisplay((score) => score + 3)
+                rowOfThree.forEach(square => currentColorArrangement[square] = 'rgba(0, 0, 0, 0)')
+                //return true
+            }
+        }
+    }
+
+    
     const createBoard = () => {
         const randomColorArrangement = [];
         for (let i = 0; i < width * width; i++) {
@@ -29,7 +93,18 @@ export default function App() {
 
     useEffect(() => {
         createBoard();
-    },[width])
+    }, [width]);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            checkForColumnOfFour();
+            checkForRowOfFour();
+            checkForColumnOfThree();
+            checkForRowOfThree();
+            setCurrentColorArrangement([...currentColorArrangement]);
+        }, 100)
+        return () => clearInterval(timer);
+    }, [checkForColumnOfFour, checkForRowOfFour, checkForColumnOfThree, checkForRowOfThree, currentColorArrangement]);
 
     //console.log(currentColorArrangement);
 
@@ -46,6 +121,7 @@ export default function App() {
                             height: (windowWidth-20)/width,
                             borderRadius: 30,
                             margin: 1,
+                            
                         }}
                     />
                     
